21599662fb37d60061d75e44e8adaaaf73a3ffae
abdallh <abdallhadly6@gmail.com>
2022-07-06 17:47:46 +0200

    final commit

---
+++ job/models.py
@@ -0,0 +1,58 @@
+from tkinter import CASCADE
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+from django.db import models
+
+# variables used
+JOB_TYPE = (
+     ("Full time","Full time"),
+     ("Part time","Part time"),
+)
+
+def upload_image(instance,filename):
+    imagename , extension = filename.split(".")
+    return ("jobs/%s.%s"%(instance.id , extension))
+
+# Create your models here.
+class Job(models.Model):
+    owner = models.ForeignKey(User , related_name='job_owner' , on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    job_type = models.CharField(max_length=15 , choices=JOB_TYPE)
+    description = models.TextField(max_length=1000)
+    published_at = models.DateTimeField(auto_now=True)
+    vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experince = models.IntegerField(default=1)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE) # we but ctegory in '' because class is under me
+    image = models.ImageField(upload_to = upload_image)
+    slug = models.SlugField(blank=True,null=True)
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args , **kwargs):
+        self.slug = slugify(self.title)
+        super(Job,self).save(*args  , **kwargs)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Apply(models.Model):
+    job = models.ForeignKey(Job, related_name='apply_job', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    website = models.URLField()
+    cv = models.FileField(upload_to= 'apply')
+    cover_letter = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    
---
+++ job/urls.py
@@ -0,0 +1,21 @@
+from argparse import Namespace
+from django.urls import path, include
+from . import views
+from . import api 
+
+app_name = 'job'
+
+urlpatterns = [
+    path('', views.job_list , name='job_list'),
+    path('add', views.add_job , name='add_job'),
+    path('<str:slug>', views.job_details , name='job_details'),
+
+    ## api
+    path('api/jobs',api.job_list_api , name='job_list_api'),
+    path('api/jobs/<int:id>',api.job_detail_api , name='job_detail_api'),
+
+    ## class based views
+    path('api/v2/jobs',api.JobListApi.as_view() , name='job_list_api_c'),
+    path('api/v2/jobs/<int:id>',api.JobDetail.as_view() , name='job_detail_api_c'),
+    
+]
